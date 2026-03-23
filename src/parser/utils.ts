f9882133fc9e0f1d211dcfa7aaa796fd6e1c5e3b
Mohammad Afaneh <mafaneh@gmail.com>
2026-03-22 21:48:30 -0400

    fix: strip all ANSI escape sequences, not just color codes

--- src/parser/utils.ts
+++ src/parser/utils.ts
@@ -1,3 +1,4 @@
-// Strip ANSI escape codes (color codes from RTT backends)
+// Strip all ANSI escape sequences: colors (\e[...m), cursor positioning (\e[r;cH),
+// screen clearing (\e[2J), cursor visibility (\e[?25l), and other CSI sequences.
 // eslint-disable-next-line no-control-regex
-export const ANSI_RE = /\x1b\[[0-9;]*m/g;
+export const ANSI_RE = /\x1b\[[\d;]*[A-Za-z]|\x1b\[\?\d+[hl]/g;
